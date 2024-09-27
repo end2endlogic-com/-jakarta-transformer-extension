b0abf13f4b97e040a130edc29e599bf7b0a5ad96
Michael Ingardia <mingardia@end2endlogic.com>
2024-09-27 15:02:58 -0400

    fix some files

--- deployment/target/generated-sources/annotations/com/end2endlogic/jakarta/transformer/extension/deployment/JakartaTransformerExtensionProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package com.end2endlogic.jakarta.transformer.extension.deployment;
-public final class JakartaTransformerExtensionProcessor$$accessor {
-    private JakartaTransformerExtensionProcessor$$accessor() {}
-    public static Object construct() {
-        return new JakartaTransformerExtensionProcessor();
-    }
-}
